7af03523e0590989473d6dfb8b2485543d4a7793
chenhuafeng888 <144212482+chenhuafeng888@users.noreply.github.com>
2023-10-10 16:35:40 +0800

    Add files via upload

--- robot.py
+++ robot.py
@@ -12,8 +12,7 @@ if len(sys.argv) > 2:
     file_path = sys.argv[2]
 
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-#model = AutoModel.from_pretrained(model_name, trust_remote_code=True).half().cuda()
-model = AutoModel.from_pretrained(model_name, trust_remote_code=True).cuda()
+model = AutoModel.from_pretrained(model_name, trust_remote_code=True).half().cuda()
 model = model.eval()
 print("模型已加载：" + model_name)
 
@@ -27,10 +26,14 @@ while True:
     # 提示用户输入查询字符串
     query = input("请输入查询字符串：")
 
+    #使用以下上下文来回答最后的问题。如果你不知道答案，就说你不知道，不要尝试编造答案。 Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    #你作为有经验的保险顾问，请基于上面内容进行充分理解并回答用户问题，问：我现在已经95岁了，能不能投保？答：
     response, history = model.chat(tokenizer, document_text + "。\n" + query, history=[])
-    #response, history = model.chat(tokenizer, document_text + "\n请根据上面内容，" + query, history=[])
-    #response, history = model.chat(tokenizer, document_text + "\n你作为聊天机器人，只能根据上面内容回复，不要有大模型幻想的内容，也不要基于你自己的知识产生内容，如果上面内容里找不到答案你就说：您的问题超出知识库范围。" + query, history=[])
+    # response, history = model.chat(tokenizer, document_text + "\n请根据上面内容，" + query, history=[])
+    # response, history = model.chat(tokenizer, document_text + "\n你作为聊天机器人，只能根据上面内容回复，不要有大模型幻想的内容，也不要基于你自己的知识产生内容，如果上面内容里找不到答案你就说：您的问题超出知识库范围。" + query, history=[])
+
     print(response)
+    print("#######################################")
 
 #!pip install protobuf transformers==4.30.2 cpm_kernels torch>=2.0 gradio mdtex2html sentencepiece accelerate
 #启动程序：python robot.py THUDM/chatglm2-6b-32k-int4 恶性肿瘤保险条款.txt
